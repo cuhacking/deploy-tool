51807b46be38dd1a542b7027ad108f01012bef40
Derek Ellis <derek@llamabagel.ca>
2020-09-25 23:36:25 -0400

    Add initial server implementation

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # deploy-tool
 
 A tool to help deploy projects as containers. Written in Rust, because.
+
+## Development
+
+Recommended development platform and tools are Linux and VS Code.
+
+To run the program for development purposes:
+```sh
+cargo run
+
+# With command line arguments
+cargo run -- --port 8888
+```
+
+To just build it:
+```sh
+cargo build
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use clap::{App, Arg};
+#[macro_use]
+extern crate rocket;
+use rocket::config::{Config, Environment};
+
+mod server;
+
 fn main() {
+    let matches = App::new("deploy-tool")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("config")
+                .long("config")
+                .short("c")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("port")
+                .long("port")
+                .short("p")
+                .value_name("PORT")
+                .help("Sets a custom port for the tool to listen on")
+                .takes_value(true),
+        )
+        .get_matches();
+    let config = matches.value_of("config").unwrap_or("default.conf");
+    let port = matches
+        .value_of("port")
+        .unwrap_or("6601")
+        .parse::<u16>()
+        .unwrap();
+
+    println!("Value for config: {}", config);
+
     println!("Hello, world!");
+
+    let config = Config::build(Environment::Staging)
+        .port(port)
+        .finalize()
+        .unwrap();
+
+    rocket::custom(config)
+        .mount("/", routes![server::index])
+        .launch();
 }
